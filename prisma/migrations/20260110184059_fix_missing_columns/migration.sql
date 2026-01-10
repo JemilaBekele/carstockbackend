0076a3e69369250d64e6855cfaa98349cf07ad98
Jemila Bekele <jemilabekele17@gmail.com>
2026-01-10 21:41:03 +0300

    sd

---
+++ prisma/migrations/20260110184059_fix_missing_columns/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isChecked` on the `stock_corrections` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `stock_corrections` DROP COLUMN `isChecked`;
