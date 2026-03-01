ad4d2818f7e399cb24345c5d69fa0c9e7f8dea8a
Jemila Bekele <jemilabekele17@gmail.com>
2026-03-01 08:19:44 +0300

    pass

--- src/controllers/SellStockCorrect.controller.js
+++ src/controllers/SellStockCorrect.controller.js
@@ -8,7 +8,7 @@ const createSellStockCorrection = catchAsync(async (req, res) => {
   const sellStockCorrection =
     await sellStockCorrectionService.createSellStockCorrection(
       req.body,
-      'f39cac5c-3f83-4f55-8012-d55a9a812982',
+      req.user.id,
     );
   res.status(httpStatus.CREATED).send({
     success: true,
@@ -103,7 +103,7 @@ const approveSellStockCorrection = catchAsync(async (req, res) => {
   const sellStockCorrection =
     await sellStockCorrectionService.approveSellStockCorrection(
       req.params.id,
-      '0d0f65fe-d2c2-46f9-b787-e2164f8a773c',
+      req.user.id,
       deliveredItemIds || [], // Pass the deliveredItemIds array from request body
     );
 
